f18d00d9008b43a0aee6331a54e9a09650da3e9f
Dubas14 <ev.hanzha@gmail.com>
2025-06-04 19:40:38 +0300

    Fix: add visibility to HOME constant

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 
 class RouteServiceProvider extends ServiceProvider
 {
-    const HOME = '/';
+    public const HOME = '/home';
 
     public function boot()
     {
