9afd208af9084255356999e2c983f5bff99ab86d
Євгеній Dubas <ev.hanzha@gmail.com>
2025-06-09 13:39:39 +0000

    Вирішено конфлікт у web.php: додано forceScheme для продакшн

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
@@ -20,4 +19,7 @@ export default defineConfig({
         }),
         vue(),
     ],
+    server: {
+        https: true
+    }
 });
