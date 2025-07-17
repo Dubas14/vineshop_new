20ddaa5ab8251d9ff895ad2a9a7c80ecf94b5701
Євгеній Ганжа <144109511+Dubas14@users.noreply.github.com>
2025-07-17 11:14:13 +0300

    Consolidate product migrations

--- database/migrations/2025_06_02_155656_create_products_table.php
+++ database/migrations/2025_06_02_155656_create_products_table.php
@@ -13,12 +13,37 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
             $table->string('name');
             $table->string('slug')->unique();
             $table->text('description')->nullable();
             $table->decimal('price', 8, 2);
+            $table->boolean('is_active')->default(true);
             $table->string('image')->nullable();
+
+            $table->string('sku')->unique()->nullable();
+            $table->string('supplier_code')->nullable();
+            $table->string('brand')->nullable();
+            $table->string('manufacturer')->nullable();
+            $table->string('country')->nullable();
+            $table->decimal('purchase_price', 10, 2)->nullable();
+            $table->decimal('sale_price', 10, 2)->nullable();
+            $table->integer('quantity')->nullable();
+            $table->integer('multiplicity')->nullable();
+
+            $table->string('region')->nullable();
+            $table->string('classification')->nullable();
+            $table->string('type')->nullable();
+            $table->string('package_type')->nullable();
+            $table->string('color')->nullable();
+            $table->string('sugar_content')->nullable();
+            $table->integer('volume')->nullable();
+            $table->string('sort')->nullable();
+            $table->text('taste')->nullable();
+            $table->text('aroma')->nullable();
+            $table->text('pairing')->nullable();
+            $table->decimal('old_price', 10, 2)->nullable();
+
             $table->timestamps();
         });
     }
